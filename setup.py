5263be60cbfe511fc5e1646dc3f6bd5f2398dc6e
ed <anax@hotmail.co.uk>
2022-10-08 12:55:15 +0100

    updated package root, setup.py, exports

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pydantic-fetch",
-    version="0.0.1",
+    version="0.0.2",
     author="ed",
     author_email="ed@bayis.co.uk",
     description="Extension of pydantic models for HTTP send/recieve",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/bayinfosys/pydantic-fetch",
-    packages=["pydantic_fetch"],
+    packages=["pydantic-fetch"],
     package_data={},
     classifiers=[
         "Programming Language :: Python :: 3",
